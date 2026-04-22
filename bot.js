528f7504858fa13a2a3250497af35c49063d5cdc
sY4nis <niyu3131@gmail.com>
2026-04-23 00:04:09 +0200

    listening for !dt

--- bot.js
+++ bot.js
@@ -7,15 +7,30 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 //creer le bot
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 client.once('clientReady', () => {
-  console.log(`Connecté en tant que ${client.user.tag} !`);
+    console.log(`Connecté en tant que ${client.user.tag} !`);
+});
+
+//ecoute h24 
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+    const args = message.content.split(' ');
+
+    //minuscule pour que ça fonctionne avec n'importe quel dt
+    const command = args[0].toLowerCase();
+
+    if (command === '!dt') {
+        const pokemon = args[1];
+        console.log(`Requested pokemon : ${pokemon}`);
+        console.log(message.content);
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
