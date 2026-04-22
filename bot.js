5d37260c7f61858c4da6207c1645429680162bf2
sY4nis <niyu3131@gmail.com>
2026-04-23 01:45:09 +0200

    fetch pokedex.ts from git + parse

--- bot.js
+++ bot.js
@@ -1,9 +1,44 @@
 // permet d'acceder au token du bot 
 require('dotenv').config();
+const axios = require('axios');
 
 //charge discord.js
 const { Client, GatewayIntentBits } = require('discord.js');
 
+
+async function parsePokemon(fileContent, name){
+    const pokemon = fileContent;
+    const position = pokemon.indexOf(`\t${name}: {`);
+    console.log(`position trouvée : ${position}`);
+    if(position == -1){
+        console.log(`${name} is not a pokemon`)
+        return null;
+    }
+    let end = 0;
+    let counter = 0;
+    let started = false;
+    for (let i = position; i < fileContent.length; i++) {
+        if (fileContent[i] == '{'){
+            counter +=1;
+            started = true;
+        }
+        if (fileContent[i] == '}'){
+            counter -=1;
+        }
+        if (started && counter === 0 && i > position) {
+            end = i;
+            break;
+        }
+    }
+    return fileContent.substring(position, end);
+}
+
+async function fetchPokedex() {
+    const response = await axios.get(process.env.POKEDEX_URL, {
+        headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` }
+    });
+    return response.data;
+}
 
 //creer le bot
 const client = new Client({
@@ -19,7 +54,7 @@ client.once('clientReady', () => {
 });
 
 //ecoute h24 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async(message) => {
     if (message.author.bot) return;
     const args = message.content.split(' ');
 
@@ -27,9 +62,11 @@ client.on('messageCreate', (message) => {
     const command = args[0].toLowerCase();
 
     if (command === '!dt') {
-        const pokemon = args[1];
-        console.log(`Requested pokemon : ${pokemon}`);
-        console.log(message.content);
+        const fileContent = await fetchPokedex();
+        const result = await parsePokemon(fileContent, args[1]);
+        let req = args[1];
+        console.log(`Requested pokemon : ${req}`);
+        console.log(`${result}`);
     }
 });
 
